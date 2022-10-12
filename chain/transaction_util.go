ae449bb70a9f329750ec67da2b6bf070fb270791
Mohammed Sohail <sohailsameja@gmail.com>
2022-10-12 10:15:00 +0000

    add: build gas transfer tx
    - TODO: missing tests
    - docs: update BuildKitabuTx to reflect contract exec type

--- chain/transaction_util.go
+++ chain/transaction_util.go
@@ -33,13 +33,29 @@ func (p *Provider) NetworkNonce(ctx context.Context, address common.Address) (ui
 	return nonce, nil
 }
 
-// BuildKitabuTx returns a signed transaction compatible with Kitabu chain
+// BuildKitabuTx returns a signed transaction compatible with Kitabu chain, it is specific to contrtact execution
 func (p *Provider) BuildKitabuTx(privateKey *ecdsa.PrivateKey, txData TransactionData) (*types.Transaction, error) {
 	tx, err := types.SignNewTx(privateKey, p.Signer, &types.LegacyTx{
 		To:       &txData.To,
 		Nonce:    txData.Nonce,
 		Data:     txData.InputData,
 		Gas:      txData.GasLimit,
+		GasPrice: big.NewInt(1),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return tx, nil
+}
+
+// BuildGasTransferTx returns a signed transaction compatible with Kitabu chain, it is specific to gas transfers
+func (p *Provider) BuildGasTransferTx(privateKey *ecdsa.PrivateKey, txData TransactionData, value *big.Int) (*types.Transaction, error) {
+	tx, err := types.SignNewTx(privateKey, p.Signer, &types.LegacyTx{
+		Value:    value,
+		To:       &txData.To,
+		Nonce:    txData.Nonce,
+		Gas:      21000,
 		GasPrice: big.NewInt(1),
 	})
 	if err != nil {
